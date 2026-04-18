29a2c8ca4a42d9f2da5d8c69b815eb49e59dcfd9
C R <ratschance@gmail.com>
2026-04-18 12:29:24 -0500

    chore: updates

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -38,6 +38,7 @@
     python3
     talosctl
     tmux
+    tree-sitter
     virtualenv
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
