0959f2a2745c4136cffdd9dc1cdfa01f96fd2f8f
C R <ratschance@gmail.com>
2026-08-09 15:04:46 -0500

    zsh: nixify

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -15,12 +15,7 @@
   # release notes.
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
     aerospace
     btop
     bat
@@ -83,23 +78,126 @@
     # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/cr/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+
+    sessionVariables = {
+      STARSHIP_CONFIG = "${config.home.homeDirectory}/.config/starship.toml";
+      EDITOR = "nvim";
+      PATH = "$HOME/bin:$HOME/.local/bin:$HOME/go/bin:$HOME/.cargo/bin:$PATH";
+    };
+
+    shellAliases = {
+      ls = "lsd";
+      l = "lsd -l";
+      la = "lsd -A";
+      ll = "lsd -AlhGF";
+      lt = "lsd --tree";
+      lg = "lazygit";
+      k = "kubecolor";
+      vim = "nvim";
+      tm = "tmux new -s m || tmux a -t m";
+    };
+
+    history = {
+      expireDuplicatesFirst = true;
+      ignoreDups = true;
+      ignoreAllDups = true;
+      ignoreSpace = true;
+      saveNoDups = true;
+      size = 10000;
+      path = "${config.home.homeDirectory}/.zsh_history";
+    };
+
+    plugins = [
+      {
+        name = "zsh-completions";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-completions";
+          rev = "334c14d1b49ec8393292b16b2a972f20570361ac";
+          sha256 = "nrWI/EWHKPIKwFRZnm6nRK7c+9U3NHllW8B3zzAtfNs=";
+        };
+      }
+      {
+        name = "zsh-syntax-highlighting";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-syntax-highlighting";
+          rev = "c4d95591843d49838b7ad30081e7aba3135a6703";
+          sha256 = "4XXqgMaB+l8ZKRJEuxuC1yqQRFhP/0ySELsSJTvjf8k=";
+        };
+      }
+      {
+        name = "zsh-history-substring-search";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-history-substring-search";
+          rev = "14c8d2e0ffaee98f2df9850b19944f32546fdea5";
+          sha256 = "KHujL1/TM5R3m4uQh2nGVC98D6MOyCgQpyFf+8gjKR0=";
+        };
+      }
+      {
+        name = "zsh-vi-mode";
+        src = pkgs.fetchFromGitHub {
+          owner = "jeffreytse";
+          repo = "zsh-vi-mode";
+          rev = "91cafe4a09b6670cb8e761aa413e5f7b9e00816f";
+          sha256 = "5ZYcxl5sjfn1XfQ7D28Si4OXwCHHapAJSboJfNgl/5A=";
+        };
+      }
+    ];
+
+    initContent = ''
+      # Sensible setopts
+      setopt AUTO_CD
+      setopt EXTENDED_GLOB
+      setopt INTERACTIVE_COMMENTS
+
+      # Colorization
+      export CLICOLOR=1
+      export LSCOLORS="exfxcxdxbxegedabagacad"
+      zstyle ':completion:*' list-colors ""
+
+      # Basic completion styling
+      zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+      zstyle ':completion:*' menu select
+
+      if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+        . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+      fi
+
+      source <(kubectl completion zsh)
+      compdef _kubectl k kubecolor
+      source <(gh completion -s zsh)
+      source <(docker completion zsh)
+
+      # History substring search bindings
+      zmodload zsh/terminfo
+      [[ -n "$terminfo[kcuu1]" ]] && bindkey "$terminfo[kcuu1]" history-substring-search-up
+      [[ -n "$terminfo[kcud1]" ]] && bindkey "$terminfo[kcud1]" history-substring-search-down
+      bindkey '^[[A' history-substring-search-up
+      bindkey '^[[B' history-substring-search-down
+
+      bindkey -r '^[h'
+      bindkey -r '^[j'
+      bindkey -r '^[k'
+      bindkey -r '^[l'
+      bindkey -r '^[u'
+      bindkey -r '^[i'
+      bindkey -r '^[o'
+      bindkey -r '^[p'
+
+      apt-history() {
+          zcat -qf /var/log/apt/history.log* | grep -Po '^Commandline: apt install (?!.*--reinstall)\K.*'
+      }
+
+      [ -f ~/.zprofile ] && source ~/.zprofile
+      [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+      eval "$(starship init zsh)"
+    '';
   };
 
   # Let Home Manager install and manage itself.
--- setup.sh
+++ setup.sh
@@ -9,13 +9,6 @@ SCRIPT_DIR="${HOME}/projects/dotfiles"
 # Tmux setup
 [ -e "$HOME/.tmux.conf" ] || ln -s "$SCRIPT_DIR/.tmux.conf" ~/.tmux.conf
 
-# Zsh setup
-[ -e "$HOME/.zshrc" ] || ln -s "$SCRIPT_DIR/.zshrc" ~/.zshrc
-if [ ! -d "$HOME/.antigen" ]
-then
-    mkdir ~/.antigen
-    curl -L git.io/antigen > ~/.antigen/antigen.zsh
-fi
 
 # Nix setup
 if [ ! -d "/nix" ]; then
